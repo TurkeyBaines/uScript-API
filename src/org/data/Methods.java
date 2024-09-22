19efa23419631260f0f59ec19f1616245da56c47
TurkeyBaines <124253621+TurkeyBaines@users.noreply.github.com>
2024-09-22 16:39:56 +0100

    Added equipWeapon & FairyRing
    @ChangeLog 1.1

--- src/org/data/Methods.java
+++ src/org/data/Methods.java
@@ -3,6 +3,8 @@ package org.data;
 import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.callback.ClientThread;
+import org.data.database.Areas;
+import org.data.database.Objects;
 import org.data.database.Widgets;
 import simple.hooks.queries.SimpleEntityQuery;
 import simple.hooks.queries.SimpleItemQuery;
@@ -27,14 +29,17 @@ public class Methods {
     private static ClientThread clientThread;
 
     public static void teleport(String options) {
+        System.out.println("uScript > Methods\t|\tTeleporting: " + options);
         c = ClientContext.instance();
-        if (options.equals("home")) {
+        if (options.equalsIgnoreCase("home")) {
             c.magic.castHomeTeleport();
             c.sleep(1000);
             return;
         }
-        if (Widgets.x.TELEPORT_MAIN.get() == null) { Widgets.x.TELEPORT_BUTTON.get().click(0); c.sleepCondition(() -> (Widgets.x.TELEPORT_MAIN.get() != null && Widgets.x.TELEPORT_MAIN.get().visibleOnScreen()), 1000); }
-
+        if (Widgets.x.TELEPORT_MAIN.get() == null) {
+            c.menuActions.sendAction(57, -1, 10616863, 2, "Teleport", "");
+            c.sleepCondition(() -> (Widgets.x.TELEPORT_MAIN.get() != null && Widgets.x.TELEPORT_MAIN.get().visibleOnScreen()), 1000);
+        }
 
         if (Widgets.x.TELEPORT_MAIN.get().getText().equals("Teleport Panel")) {
             char[] l = options.toCharArray();
@@ -43,43 +48,29 @@ public class Methods {
                 c.sleep(250);
             }
         }
-
     }
 
     public static void equipGear(int[] itemList) {
         for (int i : itemList) {
+            if (c.inventory.populate().filter(i).isEmpty() || !c.equipment.populate().filter(i).isEmpty()) continue;
             c.inventory.populate().filter(i).next().menuAction("Wear");
             c.sleep(50);
         }
     }
 
-    public static void equipWeapon(int item) {
-        c.inventory.populate().filter(item).next().menuAction("Wield");
-        c.sleep(50);
+    public static void equipWeapon(int[] itemList) {
+        for (int i : itemList) {
+            if (c.inventory.populate().filter(i).isEmpty()) continue;
+            c.inventory.populate().filter(i).next().menuAction("Wield");
+            c.sleep(50);
+        }
     }
-
-    /*
-        Usage:
-                type:
-                     0 = Objects
-                     1 = NPCs
-                     2 = Items
-                     3 = GroundItems
-
-                id:
-                    The ID of the entity your interacting with
-
-                interaction:
-                    The text of the interaction, i.e. "Pick up", "Steal", "Talk-to"
-                        - this is CaSE-sENsItIvE!
-     */
 
     public static void walk(WorldPoint tile) {
         ClientContext c = ClientContext.instance();
         c.menuActions.step(tile);
 
     }
-
 
     public static String formatTimeRunning(long start_time) {
         long running = System.currentTimeMillis() - start_time;
@@ -118,6 +109,81 @@ public class Methods {
             }
         }
         return false;
+    }
+
+    public static void fairyRing(String path) {
+        c = ClientContext.instance();
+        long startTime = System.currentTimeMillis();
+
+        while (!Areas.Home.BANK_AREA.contains(c.players.getLocal().getLocation()) && (System.currentTimeMillis() - startTime) < 5000) {
+            Methods.teleport("11");
+            c.sleepCondition(() -> Areas.Home.BANK_AREA.contains(c.players.getLocal().getLocation()));
+        }
+
+        if (Widgets.FairyRing.MAIN.get() == null) {
+            System.out.println("Opening Fairy Ring...");
+
+            startTime = System.currentTimeMillis();
+            WorldPoint target = new WorldPoint(1951, 3271, 0);
+            while (!target.equals(c.players.getLocal().getLocation()) && ((System.currentTimeMillis() - startTime) < 5000)) {
+                walk(target);
+                c.sleep(1000);
+
+            }
+
+            Objects.Teleportation.FAIRY_RING.getQuery().next().click("Configure");
+            c.sleepCondition(() -> Widgets.FairyRing.MAIN.get() != null);
+        }
+
+        int number1 = 0, number2 = 0, number3 = 0;
+        char[] input = path.toLowerCase().toCharArray();
+        System.out.println("input: " + path.toLowerCase());
+        System.out.println(input[0]);
+        switch (input[0]) {
+            case 'a': number1 = 0; break;
+            case 'b': number1 = 3; break;
+            case 'c': number1 = 2; break;
+            case 'd': number1 = 1; break;
+        }
+        System.out.println(input[1]);
+        switch (input[1]) {
+            case 'i': number2 = 0; break;
+            case 'j': number2 = 3; break;
+            case 'k': number2 = 2; break;
+            case 'l': number2 = 1; break;
+        }
+        System.out.println(input[2]);
+        switch (input[2]) {
+            case 'p': number3 = 0; break;
+            case 'q': number3 = 3; break;
+            case 'r': number3 = 2; break;
+            case 's': number3 = 1; break;
+        }
+        System.out.println("1: " + number1 + " |2: " + number2 + " |3: " + number3 + "" );
+        c.sleepCondition(() -> Widgets.FairyRing.MAIN.get() != null, 5000);
+
+        startTime = System.currentTimeMillis();
+        while (c.varpbits.varpbit(3985) != number1 && (System.currentTimeMillis() - startTime) < 5000) {
+            Widgets.FairyRing.LEFT_MOVE.get().click(0);
+            c.sleep(600);
+        }
+        startTime = System.currentTimeMillis();
+
+        while (c.varpbits.varpbit(3986) != number2 && (System.currentTimeMillis() - startTime) < 5000) {
+            Widgets.FairyRing.MIDDLE_MOVE.get().click(0);
+            c.sleep(600);
+        }
+        startTime = System.currentTimeMillis();
+
+        while (c.varpbits.varpbit(3987) != number3 && (System.currentTimeMillis() - startTime) < 5000) {
+            Widgets.FairyRing.RIGHT_MOVE.get().click(0);
+            c.sleep(600);
+        }
+        c.sleep(1000);
+
+        c.menuActions.sendAction(57, -1, 26083354, 1, "Confirm", "");
+        c.sleep(2000);
+        
     }
 
 }
